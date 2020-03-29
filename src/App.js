ff1be0d738a6202c9f5e2f9f2cec09228ea01b6e
blaise82 <nigorjeanluc@gmail.com>
2020-03-29 22:41:59 +0200

    How to validate props with proptypes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Header from './component/header';
 import Headline from './component/headline';
 import './App.scss';
 
+const tempArr = [{
+  fName: 'Joe',
+  lName: 'Bloggs',
+  email: 'joebloggs@gmail.com',
+  age: 24,
+  onlineStatus: true
+}]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <div className="main">
-          <Headline header="Posts" desc="Click the button to render"/>
+          <Headline header="Posts" desc="Click the button to render" tempArr={tempArr}/>
         </div>
       </div>
     );
