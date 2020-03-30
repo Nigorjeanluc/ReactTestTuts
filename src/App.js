91d25c24987d7accb0bd854c5e53cb04a493e81d
blaise82 <nigorjeanluc@gmail.com>
2020-03-30 14:41:26 +0200

    How to write integration tests 4 actions using moxios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Header from './component/header';
 import Headline from './component/headline';
+import SharedButton from './component/button';
+import ListItem from './component/listItem';
+import { connect } from 'react-redux';
+import { fetchPosts } from './actions';
 import './App.scss';
 
 const tempArr = [{
@@ -12,15 +16,53 @@ const tempArr = [{
 }]
 
 class App extends Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    this.fetch = this.fetch.bind(this);
+  }
+
+  fetch() {
+    this.props.fetchPosts();
+  }
+
   render() {
+    const {posts} = this.props
+
+    const configButton = {
+      buttonText: 'Get posts',
+      emitEvent: this.fetch
+    };
+  
     return (
       <div className="App">
         <Header />
         <div className="main">
           <Headline header="Posts" desc="Click the button to render" tempArr={tempArr}/>
+          <SharedButton {...configButton} />
+          {posts.length > 0 &&
+            <div>
+              {posts.map((post, index) => {
+                const { title, body} = post;
+                const configListItem = {
+                  title,
+                  desc: body
+                };
+                return (
+                  <ListItem key={index} {...configListItem}/>
+                )
+              })}
+            </div>
+          }
         </div>
       </div>
     );
   }
 }
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  }
+}
+export default connect(mapStateToProps, {fetchPosts})(App);
